4f8996aca3b81aed8e1115e5dfb3bb3d1855f68d
headrokoko <supermerio1008@hotmail.co.jp>
2014-09-22 18:37:02 +0900

    20140922 障害物のランダム生成スクリプト作成
    スタート時に設定した範囲に設定した個数のオブジェクトを生成するスクリプトの作成

--- Assets/Code/Scripts/GameData.cs
+++ Assets/Code/Scripts/GameData.cs
@@ -3,9 +3,21 @@ using System.Collections;
 
 public class GameData : MonoBehaviour {
 
+	public int MaxPlayerHP = 100;
+	[HideInInspector]
+	public int playerHP;
+	[HideInInspector]
+	public int score;
+
+	public Texture2D StartTex;
+	public Texture2D StageSelectTex;
+	public Texture2D ResultTex;
+
+
 	// Use this for initialization
 	void Start () {
-	
+		playerHP = MaxPlayerHP;
+		score = 0;
 	}
 	
 	// Update is called once per frame
