780d2b4d7e87f5b1db94e3bc09c047c3e746ad24
headrokoko <supermerio1008@hotmail.co.jp>
2014-09-22 17:36:35 +0900

    20140922 プレイヤーの操作スクリプト作成
    A,Dでロール運動
    W,Sでピッチ運動
    Q,Eで加減速

---
+++ Assets/Code/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float BaseSpeed = 10.0f;
+	public float RollSpeed = 5.0f;
+	public float PitchSpeed = 5.0f;
+	public float MaxSpeed = 50.0f;
+	public float MinSpeed = 0.0f;
+
+	private float PlayerSpeed;
+
+	// Use this for initialization
+	void Start () {
+		PlayerSpeed = BaseSpeed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(Vector3.forward * PlayerSpeed * 0.01f);
+		//ロール運動
+		if(Input.GetKey(KeyCode.A)){
+			transform.Rotate(0,0,RollSpeed);
+		}
+		if(Input.GetKey(KeyCode.D)){
+			transform.Rotate(0,0,-RollSpeed);
+		}
+
+		//ピッチ運動
+		if(Input.GetKey(KeyCode.W)){
+			transform.Rotate(-PitchSpeed,0.0f,0.0f);
+		}
+		if(Input.GetKey(KeyCode.S)){
+			transform.Rotate(PitchSpeed,0.0f,0.0f);
+		}
+
+		//加減速
+		if(Input.GetKey(KeyCode.Q) && (PlayerSpeed < MaxSpeed)){
+			PlayerSpeed += 1.0f;
+		}
+
+		if(Input.GetKey(KeyCode.E) && (PlayerSpeed > MinSpeed)){
+			PlayerSpeed -= 1.0f;
+		}
+		
+	}
+}
