2d01e1de9482103d511f319223fe0b83747eae34
TomohikoT <idumi_toilet@yahoo.co.jp>
2014-09-25 20:54:11 +0900

    ■AIの基礎追加 Attack,Chase,Dead,Patrol

---
+++ Assets/Code/AI/AttackState.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+ 
+public class AttackState : FSMState
+{
+    public AttackState(Transform[] wp) 
+    { 
+        waypoints = wp;
+        stateID = FSMStateID.Attacking;
+        curRotSpeed = 1.0f;
+        curSpeed = 100.0f;
+        FindNextPoint();
+    }
+ 
+    public override void Reason(Transform player, Transform npc)
+    {
+        //
+        float dist = Vector3.Distance(npc.position, player.position);
+        if (dist >= 200.0f && dist < 300.0f)
+        {
+            //
+            Quaternion targetRotation = Quaternion.LookRotation(destPos - npc.position);
+            npc.rotation = Quaternion.Slerp(npc.rotation, targetRotation, Time.deltaTime * curRotSpeed);
+ 
+            //
+            npc.Translate(Vector3.forward * Time.deltaTime * curSpeed);
+ 
+            Debug.Log("Switch to Chase State");
+            npc.GetComponent<NPCController>().SetTransition(Transition.SawPlayer);
+        }
+        //
+        else if (dist >= 300.0f)
+        {
+            Debug.Log("Switch to Patrol State");
+            npc.GetComponent<NPCController>().SetTransition(Transition.LostPlayer);
+        }  
+    }
+ 
+    public override void Act(Transform player, Transform npc)
+    {
+        //
+        destPos = player.position;
+ 
+        //
+        Transform turret = npc.GetComponent<NPCController>().turret;
+        Quaternion turretRotation = Quaternion.LookRotation(destPos - turret.position);
+        turret.rotation = Quaternion.Slerp(turret.rotation, turretRotation, Time.deltaTime * curRotSpeed);
+ 
+        //
+        npc.GetComponent<NPCController>().ShootBullet();
+    }
+}
---
+++ Assets/Code/AI/NPCController.cs
@@ -0,0 +1,122 @@
+﻿using UnityEngine;
+using System.Collections;
+ 
+public class NPCController : AdvancedFSM 
+{
+    public GameObject Bullet;
+    private int health;
+ 
+    //
+    protected override void Initialize()
+    {
+        health = 100;
+ 
+        elapsedTime = 0.0f;
+        shootRate = 2.0f;
+ 
+        GameObject objPlayer = GameObject.FindGameObjectWithTag("Player");
+        playerTransform = objPlayer.transform;
+ 
+        if (!playerTransform)
+            print("プレーヤーが存在しません。タグ 'Player'　を追加してください。");
+ 
+        //　
+        turret = gameObject.transform.GetChild(0).transform;
+        bulletSpawnPoint = turret.GetChild(0).transform;
+ 
+        // 
+        ConstructFSM();
+    }
+ 
+    protected override void FSMUpdate()
+    {
+        //　
+        elapsedTime += Time.deltaTime;
+    }
+ 
+    protected override void FSMFixedUpdate()
+    {
+        CurrentState.Reason(playerTransform, transform);
+        CurrentState.Act(playerTransform, transform);
+    }
+ 
+    public void SetTransition(Transition t) 
+    { 
+        PerformTransition(t); 
+    }
+ 
+    private void ConstructFSM()
+    {
+        //
+        pointList = GameObject.FindGameObjectsWithTag("WandarPoint");
+ 
+        Transform[] waypoints = new Transform[pointList.Length];
+        int i = 0;
+        foreach(GameObject obj in pointList)
+        {
+            waypoints[i] = obj.transform;
+            i++;
+        }
+ 
+        PatrolState patrol = new PatrolState(waypoints);
+        patrol.AddTransition(Transition.SawPlayer, FSMStateID.Chasing);
+        patrol.AddTransition(Transition.NoHealth, FSMStateID.Dead);
+ 
+        ChaseState chase = new ChaseState(waypoints);
+        chase.AddTransition(Transition.LostPlayer, FSMStateID.Patrolling);
+        chase.AddTransition(Transition.ReachPlayer, FSMStateID.Attacking);
+        chase.AddTransition(Transition.NoHealth, FSMStateID.Dead);
+ 
+        AttackState attack = new AttackState(waypoints);
+        attack.AddTransition(Transition.LostPlayer, FSMStateID.Patrolling);
+        attack.AddTransition(Transition.SawPlayer, FSMStateID.Chasing);
+        attack.AddTransition(Transition.NoHealth, FSMStateID.Dead);
+ 
+        DeadState dead = new DeadState();
+        dead.AddTransition(Transition.NoHealth, FSMStateID.Dead);
+ 
+        AddFSMState(patrol);
+        AddFSMState(chase);
+        AddFSMState(attack);
+        AddFSMState(dead);
+    }
+ 
+    //　
+    void OnCollisionEnter(Collision collision)
+    {
+        //
+        if (collision.gameObject.tag == "Bullet")
+        {
+            health -= 50;
+ 
+            if (health <= 0)
+            {
+                Debug.Log("Switch to Dead State");
+                SetTransition(Transition.NoHealth);
+                Explode();
+            }
+        }
+    }
+ 
+    protected void Explode()
+    {
+        float rndX = Random.Range(10.0f, 30.0f);
+        float rndZ = Random.Range(10.0f, 30.0f);
+        for (int i = 0; i < 3; i++)
+        {
+            rigidbody.AddExplosionForce(10000.0f, transform.position - new Vector3(rndX, 10.0f, rndZ), 40.0f, 10.0f);
+            rigidbody.velocity = transform.TransformDirection(new Vector3(rndX, 20.0f, rndZ));
+        }
+ 
+        Destroy(gameObject, 1.5f);
+    }
+ 
+    public void ShootBullet()
+    {
+        if (elapsedTime >= shootRate)
+        {
+            Instantiate(Bullet, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
+            elapsedTime = 0.0f;
+        }
+    }
+}
