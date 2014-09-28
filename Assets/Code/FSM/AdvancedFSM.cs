7cb101bf2ff2711197de484480a343f92ee22aab
TomohikoT <idumi_toilet@yahoo.co.jp>
2014-09-28 19:26:19 +0900

    FSMフォルダのスペルミスの修正 + Snecesフォルダに各シーンを格納

---
+++ Assets/Code/FSM/AdvancedFSM.cs
@@ -0,0 +1,124 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+ 
+/// @http://creativecommons.org/licenses/by-sa/3.0/
+ 
+public enum Transition
+{
+    None = 0,
+    SawPlayer,
+    ReachPlayer,
+    LostPlayer,
+    NoHealth,
+}
+ 
+public enum FSMStateID
+{
+    None = 0,
+    Patrolling,
+    Chasing,
+    Attacking,
+    Dead,
+}
+ 
+public class AdvancedFSM : FSM 
+{
+    private List<FSMState> fsmStates;
+ 
+    //  
+    private FSMStateID currentStateID;
+    public FSMStateID CurrentStateID { get { return currentStateID; } }
+ 
+    private FSMState currentState;
+    public FSMState CurrentState { get { return currentState; } }
+ 
+    public AdvancedFSM()
+    {
+        fsmStates = new List<FSMState>();
+    }
+ 
+    //
+    public void AddFSMState(FSMState fsmState)
+    {
+        // 
+        if (fsmState == null)
+        {
+            Debug.LogError("FSM ERROR: Null reference is not allowed");
+        }
+ 
+        // 
+        if (fsmStates.Count == 0)
+        {
+            fsmStates.Add(fsmState);
+            currentState = fsmState;
+            currentStateID = fsmState.ID;
+            return;
+        }
+ 
+        // 
+        foreach (FSMState state in fsmStates)
+        {
+            if (state.ID == fsmState.ID)
+            {
+                Debug.LogError("FSM ERROR: 既に存在する状態をリストに追加しようとしています。");
+                return;
+            }
+        }
+ 
+        //　
+        fsmStates.Add(fsmState);
+    }
+ 
+    /// 
+    public void DeleteState(FSMStateID fsmState)
+    {
+        // 
+        if (fsmState == FSMStateID.None)
+        {
+            Debug.LogError("FSM ERROR: 不正なIDです。");
+            return;
+        }
+ 
+        // 
+        foreach (FSMState state in fsmStates)
+        {
+            if (state.ID == fsmState)
+            {
+                fsmStates.Remove(state);
+                return;
+            }
+        }
+        Debug.LogError("FSM ERROR: 指定された状態が存在しません。削除に失敗しました。");
+    }
+ 
+    //　
+    public void PerformTransition(Transition trans)
+    {
+        // 
+        if (trans == Transition.None)
+        {
+            Debug.LogError("FSM ERROR: Null遷移は不正です。");
+            return;
+        }
+ 
+        // 
+        FSMStateID id = currentState.GetOutputState(trans);
+        if (id == FSMStateID.None)
+        {
+            Debug.LogError("FSM ERROR: 現在の状態はこの遷移が指定する状態を持ちません。");
+            return;
+        }
+ 
+        // 
+        currentStateID = id;
+        foreach (FSMState state in fsmStates)
+        {
+            if (state.ID == currentStateID)
+            {
+                currentState = state;
+                break;
+            }
+        }
+    }
+}
---
+++ Assets/Code/FSM/FSMState.cs
@@ -0,0 +1,105 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+ 
+ 
+/// @http://creativecommons.org/licenses/by-sa/3.0/
+/// @http://wiki.unity3d.com/index.php?title=Finite_State_Machine
+ 
+public abstract class FSMState
+{
+    protected Dictionary<Transition, FSMStateID> map = new Dictionary<Transition, FSMStateID>();
+    protected FSMStateID stateID;
+    public FSMStateID ID { get { return stateID; } }
+    protected Vector3 destPos;
+    protected Transform[] waypoints;
+    protected float curRotSpeed;
+    protected float curSpeed;
+ 
+    public void AddTransition(Transition transition, FSMStateID id)
+    {
+        // 
+        if (transition == Transition.None || id == FSMStateID.None)
+        {
+            Debug.LogWarning("FSMState : Null transition not allowed");
+            return;
+        }
+ 
+        //
+        if (map.ContainsKey(transition))
+        {
+            Debug.LogWarning("FSMState ERROR: transition is already inside the map");
+            return;
+        }
+ 
+        map.Add(transition, id);
+        Debug.Log("Added : " + transition + " with ID : " + id);
+    }
+ 
+    /// 
+    public void DeleteTransition(Transition trans)
+    {
+        // 
+        if (trans == Transition.None)
+        {
+            Debug.LogError("FSMState ERROR: NullTransition is not allowed");
+            return;
+        }
+ 
+        // 
+        if (map.ContainsKey(trans))
+        {
+            map.Remove(trans);
+            return;
+        }
+        Debug.LogError("FSMState ERROR: 指定された遷移はリストにありません。");
+    }
+ 
+ 
+    /// 
+    public FSMStateID GetOutputState(Transition trans)
+    {
+        // 
+        if (trans == Transition.None)
+        {
+            Debug.LogError("FSMState ERROR: 不正なNull遷移です。");
+            return FSMStateID.None;
+        }
+ 
+        // 
+        if (map.ContainsKey(trans))
+        {
+            return map[trans];
+        }
+ 
+        Debug.LogError("FSMState ERROR: " + trans+ " は存在しません。");
+        return FSMStateID.None;
+    }
+ 
+    /// 
+    public abstract void Reason(Transform player, Transform npc);
+ 
+    /// 
+    public abstract void Act(Transform player, Transform npc);
+ 
+    /// 
+    public void FindNextPoint()
+    {
+        //
+        int rndIndex = Random.Range(0, waypoints.Length);
+        Vector3 rndPosition = Vector3.zero;
+        destPos = waypoints[rndIndex].position + rndPosition;
+    }
+ 
+    /// 
+    protected bool IsInCurrentRange(Transform trans, Vector3 pos)
+    {
+        float xPos = Mathf.Abs(pos.x - trans.position.x);
+        float zPos = Mathf.Abs(pos.z - trans.position.z);
+ 
+        if (xPos <= 50 && zPos <= 50)
+            return true;
+ 
+        return false;
+    }
+}
